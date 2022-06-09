f5c5d11813cdc40e3606fa051f8de6abb891a4f4
Ivooo <ivovanoverstraeten@hotmail.com>
2022-06-09 15:04:58 +0200

    update

---
+++ javascript/Exercise-weather/app.js
@@ -0,0 +1,27 @@
+function sweaterOrnot() {
+
+  let temp = document.getElementById("temp").value;
+  let greeting
+  if (temp <= 0) {
+    greeting = "Wear a sweater and a jacket";
+} else if (temp < 20) {
+    greeting = "Take a sweater with you.";
+  } else {
+    greeting = "No need to wear a sweater";
+  }
+  document.getElementById("result").innerHTML = greeting;
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
